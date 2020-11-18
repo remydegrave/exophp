da637c080637ba8e42b6fed0bbd70a040ff89af6
remydegrave <bolieve.fr@gmail.com>
2020-11-18 11:41:09 +0100

    tous les exercices en php

---
+++ partie1/exercice1/index.php
@@ -0,0 +1,21 @@
+<?php
+// Déclaration de la varibale $object et initisialisation avec la valeur "L'Objet du messsage est mystérieux..."
+$object = 'L\'Objet du message est mystérieux...';
+// Déclaration et initialisation de la variable $message
+$message = 'La valeur de ce message mystérieux mais vous n\'en saurez pas plus';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1 partie 1</title>
+</head>
+
+<body>
+    <p><?php echo $object; ?></p>
+    <p><?php echo $message; ?></p>
+</body>
+
+</html>
