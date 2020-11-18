f29b3a9371dffd776084e0bcd5096d344e582d0d
remydegrave <bolieve.fr@gmail.com>
2020-11-18 11:55:00 +0100

    exercice 2

--- partie1/exercice2/index.php
+++ partie1/exercice2/index.php
@@ -1,11 +1,24 @@
+<?php
+// Déclaration de la variable $lastname et initisialisation avec la valeur "Degrave"
+$lastname = 'Nom : Degrave';
+// Déclaration de la variable $firstname et initisialisation avec la valeur "Rémy"
+$firstname = 'Prénom : Rémy';
+// Déclaration de la variable $age et initisialisation avec la valeur "19 years old"
+$age = '19';
+?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercice 2 Partie 1</title>
 </head>
+
 <body>
-    
+    <p><?php echo $lastname; ?></p>
+    <p><?php echo $firstname; ?></p>
+    <p><?php echo $age; ?></p>
 </body>
+
 </html>
