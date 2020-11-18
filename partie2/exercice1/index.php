b07f8468f203f1357cb72f8aa9ee65c6dcba72f9
remydegrave <bolieve.fr@gmail.com>
2020-11-18 16:22:14 +0100

    exercice 1 partie 2

---
+++ partie2/exercice1/index.php
@@ -0,0 +1,22 @@
+<?php
+$age = 17;
+if ($age > 18) {
+    echo "Vous êtes majeur.";
+} else {
+    echo "Vous êtes mineur.";
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1 Partie 2</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
